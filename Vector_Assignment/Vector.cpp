8f08d1e3066144342693f0d71da8d37e6229ff79
Dylan Daniels <ddanie31@ut.utm.edu>
2024-09-26 12:27:46 -0500

    got the reserve, capacity and pushback function working, fixing the size function currently

--- Vector_Assignment/Vector.cpp
+++ Vector_Assignment/Vector.cpp
@@ -11,11 +11,15 @@ Vector::Vector(){
 }
 
 Vector::Vector(const Vector &other){
-  
   vec_ptr = other.vec_ptr;
   vec_size = other.vec_size;
   vec_capacity = other.vec_capacity;
+
+  for (int i = 0; i >= vec_capacity; i++){
+    vec_ptr[i] = other.vec_ptr[i];
+  }
   
+  delete[] other.vec_ptr;
 }
 
 Vector::~Vector(){
@@ -25,8 +29,9 @@ Vector::~Vector(){
 }
 
 Vector& Vector::operator=(const Vector &other){
-  
+  int *newArray = new int[other.vec_size];
   delete[] vec_ptr;
+  vec_ptr = newArray;
   return *this;
 
 }
@@ -42,41 +47,87 @@ int& Vector::operator[](unsigned int index){
 }
 
 void Vector::push_back(int element){
-  size();
+  vec_size = size();
+  vec_capacity = capacity();
+  //if(vec_size == 0 && vec_capacity == 0){
+    //cout << "Error must allocate memory first" << endl;
+    //}
+  //else if(vec_size == 0 && vec_capacity > vec_size){
+  vec_ptr[0] = element;
+  vec_ptr[1] = element+1;
+  vec_ptr[2] = element+2;
+  
+  cout << "withinin pushback" << endl;
+  cout << vec_ptr[0] << " " << vec_ptr[1] << " "  << vec_ptr[2] << endl;
+    //}
+  
   reserve(vec_size+1);
-  //Vector[vec_size];
-
+  vec_ptr[vec_size] = element; 
 }
 
 void Vector::reserve(int n){
-  
   if(vec_capacity == 0){
+    cout << "vec_capacity: " << vec_capacity << endl;
+    vec_capacity = n;
+    cout << "set as n, vec_capacity: " << vec_capacity << endl;
+    int *newArray = new int[n];
     delete[] vec_ptr;
-    vec_capacity += n;
+    cout << "deleted vec_ptr" << endl;
     cout << "vec_capacity = 0" << endl;
     cout << "vec_capacity: "<< vec_capacity << endl << "n: " << n << endl;
-    vec_ptr = new int[vec_capacity];
+    vec_ptr = newArray;
   }
-  if(n > vec_capacity){
+  else if(n > vec_capacity){
+    cout << "pre ptr adress: " << vec_ptr << endl;
+    cout << "pre ptr size: " << sizeof(&vec_ptr) << endl;
+
+    
+    cout << "n > vec; vec_capacity: " << vec_capacity << endl;
+    cout << "pre loop ; n: " << n << endl;
+    for(int i = vec_capacity; i < n;){
+      cout << "n: " << n << endl;
+      cout << "capacity: " << vec_capacity << endl;
+      cout << endl;
+      i = i*2;
+      vec_capacity = i;
+    }
+    cout << "n: " << n << endl;
+    cout << "capacity: " << vec_capacity << endl;
+
+    
+    int *newArray = new int[vec_capacity];
+    for (int i = 0; i < vec_capacity; i++){
+      newArray[i] = vec_ptr[i];
+    }
+    
     delete[] vec_ptr;
-    vec_capacity = n*2;
-    cout << "vec_capacity < n" << endl;
-    cout << "vec_capacity: "<< vec_capacity << endl << "n: " << n << endl;
-    vec_ptr = new int[vec_capacity*2];
+    vec_ptr = newArray;
+    cout << "post ptr adress: " << vec_ptr << endl;
+    cout << "post ptr size: " << sizeof(&vec_ptr) << endl;
   }
-  if (n <= vec_capacity){
+  else if (n <= vec_capacity){
   }
   
 }
 
 int Vector::size(){
-  
+  cout << endl << "size:" << endl << endl;
   char end = '\0';
   char current = *vec_ptr;
   int size = 0;
   while(end != current){
+    cout << "size of array: " << size << endl;
+    cout << "current char: " << current << endl;
     current = (*vec_ptr + size);
     size++;
+    
+      
+   cout << endl;
   }
   return size;
+}
+
+int Vector::capacity(){
+  return vec_capacity;
+  
 }
--- Vector_Assignment/main.cpp
+++ Vector_Assignment/main.cpp
@@ -10,12 +10,26 @@ int main() {
   
   Vector newVector;
 
+  newVector.reserve(2);
   newVector.reserve(10);
-  newVector[0] = 1;
-  int test = newVector[0];
-  int size = newVector.size();
-  cout << size;
+  //newVector.reserve(100);
+  cout << "before pushback" << endl;
+  newVector.push_back(5);
+  cout << "after pushback" << endl;
+  int test = newVector.capacity();
+  //int test2 = newVector.size();
+  cout << test << " "
+    //<< test2
+       << endl;
+  
+  //newVector[0] = 1;
+  //int test = newVector[0];
+  //int size = newVector.size();
+  //cout << size;
   //cout << test << endl;
+  //newVector.capacity();
+
+  
   //loadData(newVector);
   return 0;
 }
